777c903491ee4422ed4854fb7d2c3f65cb2fe547
Riley Kopp <koppril7@gmail.com>
2020-09-10 22:08:24 -0600

    Added re-entrant data

--- program1/darts.c
+++ program1/darts.c
@@ -2,7 +2,6 @@
 
 void darts(long long int n)
 {
-    srandom(0);
     long long int index     = 0;
     long long int hit_count = 0;
     double        x         = 0;
@@ -12,10 +11,11 @@ void darts(long long int n)
     #pragma omp parallel for num_threads(omp_get_num_procs()) default(none) shared(hit_count, n) private(x, y, index) 
     for(index = 0; index < n; index++)
     {
-        x = random() / (double) RAND_MAX;
-        y = random() / (double) RAND_MAX;
+	srandom_r(omp_get_thread_num());
+        x = random_r() / (double) RAND_MAX;
+        y = random_r() / (double) RAND_MAX;
 
-        if(x * x + y * y < 1)
+        if(x * x + y * y <= 1)
         {
             #pragma omp critical
             hit_count++;
