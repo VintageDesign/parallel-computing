576cd4c62f01185e9bfcf6ce4ba7f99488a8ffb5
Riley Kopp <koppril7@gmail.com>
2020-10-16 10:02:10 -0600

    Added timing and random sized graphs

--- exam1/main.cu
+++ exam1/main.cu
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <limits.h>
+#include <time.h>
 
 #include "floyd_s.h"
 #include "floyd_cuda.h"
@@ -16,6 +17,8 @@ typedef struct command_args {
     bool run_serial;
     bool run_omp;
     bool run_cuda;
+    bool generate_random_graph;
+    int  graph_size;
     char * file_name;
 } command_args_t;
 
@@ -24,6 +27,7 @@ void print_usage(void);
 
 void parse_adj(FILE * file_handle_p, int * size, int *** adj, int *** adj_answer);
 
+void generate_graph(int size, int *** adj);
 
 int main(int argc, char ** argv)
 {
@@ -37,42 +41,61 @@ int main(int argc, char ** argv)
 
     command_args_t parsed_args = parse_args(argc, argv);
 
-    printf("Opening File: %s...", parsed_args.file_name);
-    file_handle_p = fopen(parsed_args.file_name, "r");
-    if(file_handle_p == NULL)
+    if(parsed_args.generate_random_graph)
     {
-        printf("\nERROR! Could not open file %s\n", parsed_args.file_name);
-        exit(1);
+        adj_size = parsed_args.graph_size;
+        generate_graph(adj_size, &adj);
     }
-    printf("Done\n");
+    else
+    {
+
+        printf("Opening File: %s...", parsed_args.file_name);
+        file_handle_p = fopen(parsed_args.file_name, "r");
+        if(file_handle_p == NULL)
+        {
+            printf("\nERROR! Could not open file %s\n", parsed_args.file_name);
+            exit(1);
+        }
+        printf("Done\n");
 
 
-    printf("Parsing File....");
-    parse_adj(file_handle_p, &adj_size, &adj, &adj_answer);
-    printf("Done!\n");
+        printf("Parsing File....");
+        parse_adj(file_handle_p, &adj_size, &adj, &adj_answer);
+        printf("Done!\n");
+        fclose(file_handle_p);
+
+    }
 
     printf("Graph size: %d nodes\n", adj_size);
     
+    struct timespec start, end;
+
 
     if(parsed_args.run_serial)
     {
         printf("Serial: ");
         copy_adj(adj_size, adj, &solution);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         floyd_serial(adj_size, &solution);
-        if (!check_answer(adj_size, adj_answer, solution))
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        if(!parsed_args.generate_random_graph)
         {
-            printf("Fail\n");
-            printf("Solution:\n");
-            print_matrix(adj_size, adj_answer);
-            printf("Calculated:\n");
-            print_matrix(adj_size, solution);
-            printf("Starting:\n");
-            print_matrix(adj_size, adj);
+            if (!check_answer(adj_size, adj_answer, solution))
+            {
+                printf("Fail\n");
+                printf("Solution:\n");
+                print_matrix(adj_size, adj_answer);
+                printf("Calculated:\n");
+                print_matrix(adj_size, solution);
+                printf("Starting:\n");
+                print_matrix(adj_size, adj);
+            }
+            else
+            {
+                printf("Pass\n");
+            }
         }
-        else
-        {
-            printf("Pass\n");
-        }
+        printf("Elapsed Time: %lu miliseconds\n", (end.tv_sec - start.tv_sec) * 1000 +  (end.tv_nsec - start.tv_nsec)/1000000);
 
     }
 
@@ -80,15 +103,21 @@ int main(int argc, char ** argv)
     {
         printf("OpenMP: ");
         copy_adj(adj_size, adj, &solution);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         floyd_omp(adj_size, &solution);
-        if (!check_answer(adj_size, adj_answer, solution))
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        if(!parsed_args.generate_random_graph)
         {
-            printf("Fail\n");
+            if (!check_answer(adj_size, adj_answer, solution))
+            {
+                printf("Fail\n");
+            }
+            else
+            {
+                printf("Pass\n");
+            }
         }
-        else
-        {
-            printf("Pass\n");
-        }
+        printf("Elapsed Time: %lu miliseconds\n", (end.tv_sec - start.tv_sec) * 1000 +  (end.tv_nsec - start.tv_nsec)/1000000);
 
     }
 
@@ -96,26 +125,31 @@ int main(int argc, char ** argv)
     {
         printf("Cuda: ");
         copy_adj(adj_size, adj, &solution);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
         floyd_cuda(adj_size, &solution);
-        if (!check_answer(adj_size, adj_answer, solution))
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+        if(!parsed_args.generate_random_graph)
         {
-            printf("Fail\n");
-            printf("Solution:\n");
-            print_matrix(adj_size, adj_answer);
-            printf("Calculated:\n");
-            print_matrix(adj_size, solution);
-            printf("Starting:\n");
-            print_matrix(adj_size, adj);
+            if (!check_answer(adj_size, adj_answer, solution))
+            {
+                printf("Fail\n");
+                printf("Solution:\n");
+                print_matrix(adj_size, adj_answer);
+                printf("Calculated:\n");
+                print_matrix(adj_size, solution);
+                printf("Starting:\n");
+                print_matrix(adj_size, adj);
+            }
+            else
+            {
+                printf("Pass\n");
+            }
         }
-        else
-        {
-            printf("Pass\n");
-        }
+        printf("Elapsed Time: %lu miliseconds\n", (end.tv_sec - start.tv_sec) * 1000 +  (end.tv_nsec - start.tv_nsec)/1000000);
     }
    
 
     // Clean up...
-    fclose(file_handle_p);
     free(adj);
     free(adj_answer);
 
@@ -142,12 +176,44 @@ command_args_t parse_args(uint8_t argc, char ** argv)
         retval.run_serial = true;
         retval.run_omp = true;
         retval.run_cuda = true;
+        retval.generate_random_graph = false;
         retval.file_name = argv[1];
     }
 
     else if(argc == 3)
     {
         retval.file_name = argv[2];
+        retval.generate_random_graph = false;
+
+        if(strcmp("-s", argv[1]) == 0)
+        {
+            retval.run_serial = true;
+            retval.run_omp = false;
+            retval.run_cuda = false;
+        }
+        else if(strcmp("-o", argv[1]) == 0)
+        {
+            retval.run_serial = false;
+            retval.run_omp = true;
+            retval.run_cuda = false;
+        }
+        else if(strcmp("-c", argv[1]) == 0)
+        {
+            retval.run_serial = false;
+            retval.run_omp = false;
+            retval.run_cuda = true;
+        }
+        else
+        {
+            printf("ERROR! Could not parse argument \"%s\"\n", argv[1]);
+            print_usage();
+            exit(1);
+        }
+    }
+    else if(argc == 4)
+    {
+        retval.generate_random_graph = true;
+        retval.graph_size = atoi(argv[3]);
 
         if(strcmp("-s", argv[1]) == 0)
         {
@@ -181,6 +247,36 @@ command_args_t parse_args(uint8_t argc, char ** argv)
 void print_usage(void)
 {
     // TODO
+}
+
+void generate_graph(int size
+        , int *** adj)
+{
+    int edge_roll = 0;
+    (*adj)        = (int **) calloc(size, sizeof(int *));
+    for(int index = 0; index < size; index++)
+    {
+        (*adj)[index]        = (int *) calloc(size, sizeof(int));
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if( i != j)
+            {
+                edge_roll = random() % 100 + 1;
+                if (edge_roll < 33)
+                {
+                    (*adj)[i][j] = INF; 
+                }
+                else
+                {
+                    (*adj)[i][j] = random() % 50 + 1;
+                }
+            }
+        }
+    }
 }
 
 
