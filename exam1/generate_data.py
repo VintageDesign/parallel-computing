d6ca906c448bd26dbf66516c07b55cb645297a10
Riley Kopp <koppril7@gmail.com>
2020-10-18 11:07:28 -0600

    Added table printing to the script

--- exam1/generate_data.py
+++ exam1/generate_data.py
@@ -8,9 +8,11 @@ import re
 cuda_data = []
 omp_data = []
 serial_data = []
+index = 0
 run_rng = 3001
 step = 500
 
+print("N", "Cuda", "OMP", "Serial")
 for i in range(0, run_rng, step):
     stream =  os.popen("./run_exam_1 -c -r " + str(i))
     cuda_data.append(int(re.findall("[0-9]+", stream.read())[1]))
@@ -18,6 +20,11 @@ for i in range(0, run_rng, step):
     omp_data.append(int(re.findall("[0-9]+", stream.read())[1]))
     stream =  os.popen("./run_exam_1 -s -r " + str(i))
     serial_data.append(int(re.findall("[0-9]+", stream.read())[1]))
+
+    print(index, cuda_data[index], omp_data[index], serial_data[index])
+    index +=1
+
+
 
 plt.plot(range(0, run_rng, step), cuda_data,'o', color='green', label="Cuda")
 plt.plot(range(0, run_rng, step), omp_data,'o', color='red', label="OpenMP")
