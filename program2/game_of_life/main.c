886006156c547e9ea8a305cc813ee3d612264d54
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2020-11-12 16:54:21 -0700

    Wasted far too many hours of my life trying to get MPI_Gather to work.

--- program2/game_of_life/main.c
+++ program2/game_of_life/main.c
@@ -2,80 +2,89 @@
 #include <stdio.h>
 #include "utils.h"
 
+int ** get_board(int rank, int * row, int ** board, command_args_t config)
+{
+   MPI_Status status;
+
+   if(rank == 0)
+   {
+      for(int i = 1; i < config.row_size; i++)
+      {
+         MPI_Recv(board[i], config.col_size, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      }
+   }
+   else
+   {
+      MPI_Send(row, config.col_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+   }
+
+   return board;
+}
+
 int main(int argc, char * argv[]){
    command_args_t arguments = parse_args(argc, argv);
    int num_tasks = 0;
    int rank = 0;
-   int tile_value = 0;
    MPI_Status status;
-
-   int ** board;
-
-   // One bigass vector for the game of life
-   board = malloc(sizeof(int) * arguments.row_size * arguments.col_size);
-   for( int i = 0; i <= arguments.row_size * arguments.col_size; i++)
-   {
-      board[i] = malloc(sizeof(int));
-   }
-
    MPI_Init(&argc, &argv);
 
    MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
-   printf("%d\n", num_tasks);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   // Initialize the board
-   if( rank == 0)
+   int * row;
+   int ** board;
+
+   if(rank == 0)
    {
-      int live_cell_count = 0;
-      int value = 0;
-      for( int i = 1; i <= arguments.row_size * arguments.col_size; i++)
+      board = malloc(sizeof(int) * arguments.row_size );
+      for( int i = 0; i < arguments.row_size ; i++)
       {
-         if(live_cell_count <= arguments.starting_cells)
-         {
-            value = rand() % 2;
-            live_cell_count += value;
+         board[i] = malloc(sizeof(int) * arguments.col_size);
+      }
+   }
+
+   row = malloc(sizeof(int) * arguments.col_size);
+   MPI_Barrier(MPI_COMM_WORLD);
+
+   // Init board
+   if(rank == 0)
+   {
+      int rand_x;
+      int rand_y;
+      for(int index = 0; index < arguments.starting_cells; index++)
+      {
+         do {
+            rand_x = rand() % arguments.row_size;
+            rand_y = rand() % arguments.col_size;
          }
-         MPI_Send(&value, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+         while(board[rand_x][rand_y] != 0);
+
+         board[rand_x][rand_y] = 1;
       }
 
+      row = board[0];
+
+      for(int index = 1; index < arguments.row_size; index++)
+      {
+         MPI_Send(board[index], arguments.col_size, MPI_INT, index, 0, MPI_COMM_WORLD);
+      }
    }
+
    else
    {
-      MPI_Recv(&tile_value, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+      MPI_Recv(row, arguments.col_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+      printf("%d got Row\n",rank);
    }
 
+   board = get_board(rank, row, board, arguments);
    MPI_Barrier(MPI_COMM_WORLD);
 
-
-   MPI_Gather(&tile_value, 1, MPI_INT, &board, (arguments.row_size * arguments.col_size), MPI_INT, 0, MPI_COMM_WORLD);
-
-
-
-   //    int ** board = generate_game(arguments);
-
-
-   if( rank == 0)
+   if(rank == 0)
    {
-      printf("Starting Board");
       print_board(board, arguments);
    }
-
    MPI_Barrier(MPI_COMM_WORLD);
-   /*
-   for(int generation = 0; generation < arguments.epochs; generation ++)
-   {
-      board = run_game(board, arguments);
 
-      if(generation % arguments.print_generation == 0)
-      {
-         printf("Generation: %d", generation);
-         print_board(board, arguments);
-      }
-
-   }
-
-   */
 
    MPI_Finalize();
 
